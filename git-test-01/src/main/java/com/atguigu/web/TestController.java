b035cc7b5fd4c1920c777b3eba0e5973f91f0dad
xiaoqiang <xiaoqiang@163.com>
2022-05-15 21:58:59 +0800

    idea 首次提交

---
+++ git-test-01/src/main/java/com/atguigu/web/TestController.java
@@ -0,0 +1,18 @@
+package com.atguigu.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author rrqstart
+ * @version v1.0
+ * @date 2022-05-15-下午 9:53
+ * @Description
+ */
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    public String test() {
+        return "Hello Git";
+    }
+}
