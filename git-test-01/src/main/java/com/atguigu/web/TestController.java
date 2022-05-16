aadf811caab63e5246536b665449ac7315977643
xiaoqiang <xiaoqiang@163.com>
2022-05-16 16:46:35 +0800

    idea 第五次提交

--- git-test-01/src/main/java/com/atguigu/web/TestController.java
+++ git-test-01/src/main/java/com/atguigu/web/TestController.java
@@ -14,6 +14,7 @@ public class TestController {
     @RequestMapping("/test")
     public String test() {
         System.out.println("hot-fix");
+        System.out.println("master");
         Integer age = 18;
         String name = "Tom";
         return "Hello Git " + age + " " + name;
